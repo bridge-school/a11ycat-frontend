73298099eee6ea2396e85d9f269450a0de086754
Jasper Walton <jasper.lyn.walton@gmail.com>
2019-02-24 14:07:39 -0500

    initial redux

---
+++ src/store/index.js
@@ -0,0 +1,9 @@
+import { createStore, applyMiddleware } from 'redux';
+import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import { rootReducer } from './reducers';
+
+export const Store = createStore(
+  rootReducer, 
+  applyMiddleware(logger, thunk));
+  
