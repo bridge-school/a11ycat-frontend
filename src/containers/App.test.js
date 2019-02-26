74f9018b8ba0871218594339227630f83f8b337a
Jasper Walton <32462443+jaswalt@users.noreply.github.com>
2019-02-26 13:09:14 -0500

    Update App.test.js

--- src/containers/App.test.js
+++ src/containers/App.test.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
