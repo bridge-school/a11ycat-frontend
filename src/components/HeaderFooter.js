ab9747f7d7a6bf67ddbd74021eedaad6b474a82c
choilm <choilmto@gmail.com>
2019-02-27 16:42:59 -0500

    The headers and footers are written.

---
+++ src/components/HeaderFooter.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+export const Header = (props) =>
+  <header>
+    <img src={require('../CatCallLogo.png')}
+      style={{height: 40}}></img>
+    <span>catcall.io</span>
+    <a href="">home</a>
+  </header>
+
+
+export const Footer = () =>
+  <footer>
+  </footer>
