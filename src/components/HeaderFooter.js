880d7df31e541228900fe3be01ac888432d1945e
choilm <choilmto@gmail.com>
2019-02-28 16:01:53 -0500

    removed unused logo

--- src/components/HeaderFooter.js
+++ src/components/HeaderFooter.js
@@ -3,9 +3,12 @@ import React from "react"
 export const Header = (props) =>
   <header>
     <img src={require('../CatCallLogo.png')}
-      style={{height: 40}}></img>
+      style={{height: 40}}
+      alt="catcall.io logo"></img>
     <span>catcall.io</span>
-    <a href="">home</a>
+    <a href="#" onClick={() => {
+      console.log("set visibility of components")
+    }}>home</a>
   </header>
 
 
