9cf462d2b1b1dd92d631a08045540cc7a7eafda5
Jasper Walton <32462443+jaswalt@users.noreply.github.com>
2019-02-28 20:30:26 -0500

    Select action (#4)
    * initial redux
    
    * connect App
    
    * Update App.test.js
    
    * Update App.test.js
    
    * Update App.test.js
    
    * wrap App in Provider, fix import Store path
    
    * SelectAction page initial set up and first redux actions
    
    * delete redux actions and reducer for views
    
    * remove app.test
    
    * get app.test to pass, connect main instead of app

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,14 +1,20 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { Store } from '../store';
+import Main from '../components/Main';
 import './App.css';
 import { Header, Footer } from '../components/HeaderFooter';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <Provider store={Store}>
+      <div className="App">
         <Header />
+        <Main />
         <Footer />
       </div>
+      </Provider>
     );
   }
 }
