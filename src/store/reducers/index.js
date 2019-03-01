5d25bba051cdd9c397014cf7a85160cb8e6fed26
Jasper Walton <jasper.lyn.walton@gmail.com>
2019-02-28 22:18:28 -0500

    set up routing

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,3 +1,6 @@
 import { combineReducers } from 'redux';
+import { connectRouter } from 'connected-react-router'
 
-export const rootReducer = combineReducers({});
+export default (history) => combineReducers({
+  router: connectRouter(history),
+});
