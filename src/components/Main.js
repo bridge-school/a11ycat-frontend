542e8381acf05bb52272134e13eb64f52ca63606
Jasper Walton <32462443+jaswalt@users.noreply.github.com>
2019-03-01 22:26:31 -0500

    set up routing (#5)

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
-import { SelectAction } from './views/SelectAction';
 import { connect } from 'react-redux';
+import { Route, Switch } from 'react-router';
+import { ConnectedRouter } from 'connected-react-router';
+import { history } from '../store';
+
+import { SelectAction } from './views/SelectAction';
+import { ReportIncident } from './views/ReportIncident';
+import { ViewReports } from './views/ViewReports';
 
 class Main extends Component {
   render() {
     return (
       <div className="App">
-        <SelectAction />
+        <ConnectedRouter history={history}>
+            <Switch>
+              <Route exact path="/" render={() => <SelectAction />} />
+              <Route exact path="/report-incident" render={() => <ReportIncident />} />
+              <Route exact path="/view-reports" render={() => <ViewReports />} />
+            </Switch>
+        </ConnectedRouter>
       </div>
     );
   }
