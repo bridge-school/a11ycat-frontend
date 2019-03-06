dae46437e55f66d62af9417f548d39ed58422321
Alina <alina.dolgoy@gmail.com>
2019-03-05 21:00:30 -0500

    added ref to the map div

--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -4,6 +4,7 @@ class Map extends React.Component {
     constructor(props) {
         super(props);
         const { lat, lng } = this.props.initialCenter; // this will be provided by the browser's location 
+        this.mapRef = React.createRef(); // this creates a ref that we will use for the map div
         this.state = {
             currentLocation: {
                 lat: lat,
@@ -15,7 +16,7 @@ class Map extends React.Component {
 
     render() {
         return (
-            <div>
+            <div ref={this.mapRef}>
                 Loading map...
             </div>
 
