354f9fec4e23e58c8796b9cf9ce6b7c7d08fbefe
Laura-Louise Tobin <lauralouisetobin@gmail.com>
2019-03-05 20:10:58 -0500

    Fixed conflicts -- form inputs branch up to date

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,23 +1,27 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route, Switch } from 'react-router';
-import { ConnectedRouter } from 'connected-react-router';
-import { history } from '../store';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Route, Switch } from "react-router";
+import { ConnectedRouter } from "connected-react-router";
+import { history } from "../store";
 
-import { SelectAction } from './views/SelectAction';
-import { ReportIncident } from './views/ReportIncident';
-import { ViewReports } from './views/ViewReports';
+import { SelectAction } from "./views/SelectAction";
+import { ReportIncident } from "./views/ReportIncident";
+import { ViewReports } from "./views/ViewReports";
 
 class Router extends Component {
   render() {
     return (
       <div className="App">
         <ConnectedRouter history={history}>
-            <Switch>
-              <Route exact path="/" render={() => <SelectAction />} />
-              <Route exact path="/report-incident" render={() => <ReportIncident />} />
-              <Route exact path="/view-reports" render={() => <ViewReports />} />
-            </Switch>
+          <Switch>
+            <Route exact path="/" render={() => <SelectAction />} />
+            <Route
+              exact
+              path="/report-incident"
+              render={() => <ReportIncident />}
+            />
+            <Route exact path="/view-reports" render={() => <ViewReports />} />
+          </Switch>
         </ConnectedRouter>
       </div>
     );
@@ -28,4 +32,7 @@ const mapStateToProps = store => ({});
 
 const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(Router);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Router);
