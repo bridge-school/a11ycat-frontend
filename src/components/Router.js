ec1e9dc4f9f4597e07fa4d37d57174aad24fbd2d
Jasper Walton <jasper.lyn.walton@gmail.com>
2019-03-06 10:50:11 -0500

    home page buttons route to desired page when clicked

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,18 +2,17 @@ import React from "react";
 import { connect } from "react-redux";
 import { Route, Switch } from "react-router";
 import { ConnectedRouter } from "connected-react-router";
-import { history } from "../store";
 
 import { SelectAction } from "./views/SelectAction";
 import { ReportIncident } from "./views/ReportIncident";
 import { ViewReports } from "./views/ViewReports";
 
-const Router = () => (
+const Router = ({ history }) => (
   <ConnectedRouter history={history} className="App">
     <Switch>
-      <Route exact path="/" render={() => <SelectAction />} />
-      <Route exact path="/report-incident" render={() => <ReportIncident />} />
-      <Route exact path="/view-reports" render={() => <ViewReports />} />
+      <Route exact path="/" component={SelectAction} />
+      <Route exact path="/report-incident" component={ReportIncident} />
+      <Route exact path="/view-reports" component={ViewReports} />
     </Switch>
   </ConnectedRouter>
 );
