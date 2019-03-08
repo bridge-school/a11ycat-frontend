9e387ce11a0d7f22ab7893d24e62e9a542293493
choilm <choilmto@gmail.com>
2019-03-08 11:53:14 -0500

    Format code

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,6 +1,6 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
-import { reducer as formReducer } from 'redux-form';
+import { reducer as formReducer } from "redux-form";
 
 export default history =>
   combineReducers({
