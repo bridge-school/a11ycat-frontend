e45b0eb06356e304fd3d2d27f697c426c4d6345e
choilmto <choilmto@gmail.com>
2019-03-08 11:56:21 -0500

    Redux form (#25)
    * Setup redux-form
    
    * Format code

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,7 +1,9 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
+import { reducer as formReducer } from "redux-form";
 
 export default history =>
   combineReducers({
-    router: connectRouter(history)
+    router: connectRouter(history),
+    form: formReducer
   });
