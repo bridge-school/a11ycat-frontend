ec4a3c674079043ca5b05fabd827a74403ba7956
Jasper Walton <32462443+jaswalt@users.noreply.github.com>
2019-03-09 09:23:15 -0500

    Revert "Redux form (#25)"
    This reverts commit e45b0eb06356e304fd3d2d27f697c426c4d6345e.

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,9 +1,7 @@
 import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
-import { reducer as formReducer } from "redux-form";
 
 export default history =>
   combineReducers({
-    router: connectRouter(history),
-    form: formReducer
+    router: connectRouter(history)
   });
