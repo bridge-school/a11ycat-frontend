0db10a571b4e08f4f12e716bca78fd80d22de0cb
choilm <choilmto@gmail.com>
2019-03-13 15:34:18 -0400

    Fixed formatting

--- src/store/actions/radioScaleActions.js
+++ src/store/actions/radioScaleActions.js
@@ -1,7 +1,6 @@
 import * as types from "./index";
 
-export const selectedRating = rating =>
-({
+export const selectedRating = rating => ({
   type: types.SELECTED_RATING,
   rating
-})
+});
--- src/store/reducers/radioScaleReducer.js
+++ src/store/reducers/radioScaleReducer.js
@@ -2,18 +2,18 @@ import * as types from "../actions";
 
 const initialState = {
   selectedRating: null
-}
+};
 
 export default (state = initialState, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case types.SELECTED_RATING: {
       return {
         ...state,
         selectedRating: action.selectedRating
-      }
+      };
     }
     default: {
-      return state
+      return state;
     }
   }
-}
+};
