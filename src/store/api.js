442b130f13e4b61c3df6db4da439003ebafbd887
Jasper Walton <jasper.lyn.walton@gmail.com>
2019-03-13 19:36:08 -0400

    fix api call to get address

--- src/store/api.js
+++ src/store/api.js
@@ -2,7 +2,9 @@ export const apiTransformGeoToAddress = ({ google, lat, lng }) => {
   const geocoder = new google.maps.Geocoder();
   const latlng = { lat, lng };
 
-  return geocoder.geocode({ location: latlng }, res => {
-    return res[0].formatted_address;
+  return new Promise(resolve => {
+    geocoder.geocode({ location: latlng }, resp => {
+      resolve(resp[0].formatted_address);
+    });
   });
 };
