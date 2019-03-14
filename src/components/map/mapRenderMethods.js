7dff103d35756bcbd3dc2bb68e7eb0508952d64b
Alina Dolgoy <alina.dolgoy@gmail.com>
2019-03-14 12:18:49 -0400

    Multiple markers (#44)
    * added an array in state for testing purposes and first stage of mapping through it to display the markers
    
    * added colors to the markers, and logic (that will need to be modified once we have keys from the backend) to check which color icon to display
    
    * created functions to render each a different map for view reports. next step is to create the function for creating the map for report an incident
    
    * added rending report incident map function
    
    * moved the map rending methods into a different file
    
    * cleaned up a bit and added comments
    
    * cleaned up a bit and added comments2
    
    * resolving merge conflicts

---
+++ src/components/map/mapRenderMethods.js
@@ -0,0 +1,49 @@
+import React from "react";
+import { Marker } from "google-maps-react";
+
+// renders the map with all the markers of the previous reported incidents
+// the icon of the incident the user just submitted will be displayed in a different color
+
+// if the user goes to the view reports screen straight from home page there will not be a key for a submitted report
+export const renderViewReports = latLngArray => {
+  let icon;
+  return latLngArray.map((prevReport, i) => {
+    if (prevReport.key === "1") {
+      icon = {
+        url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+      };
+    } else {
+      icon = {
+        url: "http://maps.google.com/mapfiles/ms/icons/pink-dot.png"
+      };
+    }
+    return (
+      <Marker key={i} title={i} position={prevReport.coords} icon={icon} />
+    );
+  });
+};
+
+// displays only one on (initally) user's current location and then can be moved around
+export const renderReportIncident = currentCenter => {
+  return (
+    <Marker
+      title={"MYCURRENTLOCATION"}
+      name={"MYCURRENTLOCATION"}
+      position={currentCenter}
+      icon={{
+        url: "http://maps.google.com/mapfiles/ms/icons/blue-dot.png"
+      }}
+    />
+  );
+};
+
+//
+export const whatToRender = (currentCenter, currentView, latLngArray) => {
+  if (currentView === "viewReports") {
+    return renderViewReports(latLngArray);
+  }
+  if (currentView === "reportIncident") {
+    return renderReportIncident(currentCenter);
+  }
+  return renderViewReports();
+};
